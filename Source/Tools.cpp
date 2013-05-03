e9abefe0d115138cfa457f23fa112ae6b18f731f
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2013-05-03 12:52:05 -0400

    stabilisation plug in

---
+++ Source/Tools.cpp
@@ -0,0 +1,9 @@
+//
+//  Tools.cpp
+//  ZirkOSCJUCE
+//
+//  Created by Lud's on 3/05/13.
+//
+//
+
+#include "Tools.h"
---
+++ Source/Tools.h
@@ -0,0 +1,14 @@
+//
+//  Tools.h
+//  ZirkOSCJUCE
+//
+//  Created by Lud's on 3/05/13.
+//
+//
+
+#ifndef __ZirkOSCJUCE__Tools__
+#define __ZirkOSCJUCE__Tools__
+
+#include <iostream>
+
+#endif /* defined(__ZirkOSCJUCE__Tools__) */
