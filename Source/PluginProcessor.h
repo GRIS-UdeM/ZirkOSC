c9e3e6240177a11008b574c1fc4cd2311823e911
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2013-05-10 15:55:00 -0400

    comments refactoring

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -19,6 +19,7 @@
 
 //==============================================================================
 /**
+ The processor class of the plug in
  */
 class ZirkOscjuceAudioProcessor  : public AudioProcessor,public Timer
 {
@@ -35,69 +36,91 @@ public:
     
     //bool beginGesture =false;
     //==============================================================================
+    //! Called before playback starts, to let the filter prepare itself. 
     void prepareToPlay (double sampleRate, int samplesPerBlock);
+    //! Called after playback has stopped, to let the filter free up any resources it no longer needs. 
     void releaseResources();
     //static     int receivePositionUpdate(const char *path, const char *types, lo_arg **argv, int argc, void *data, void *user_data);
+    
+    //! Renders the next block. 
     void processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages);
 
     //==============================================================================
+    //! Creates the editor and store it
     AudioProcessorEditor* createEditor();
-    
+    //! returns true if the processor has an editor
     bool hasEditor() const;
 
     //==============================================================================
+    //! Returns the name of this processor. 
     const String getName() const;
 
+    //! This must return the correct value immediately after the object has been created, and mustn't change the number of parameters later.
     int getNumParameters();
-
-
+    //! Called by the host to find out the value of one of the filter's parameters.
     float getParameter (int index);
+    //! The host will call this method to change the value of one of the filter's parameters. 
     void setParameter (int index, float newValue);
-
+    //! Returns the name of a particular parameter. 
     const String getParameterName (int index);
+    //! Returns the value of a parameter as a text string. 
     const String getParameterText (int index);
-
+    
+    //! Returns the number of input channels that the host will be sending the filter.
     const String getInputChannelName (int channelIndex) const;
+    //! Returns the number of output channels that the host will be sending the filter. 
     const String getOutputChannelName (int channelIndex) const;
+    //! Returns true if the specified channel is part of a stereo pair with its neighbour. 
     bool isInputChannelStereoPair (int index) const;
+    //!Returns true if the specified channel is part of a stereo pair with its neighbour.
     bool isOutputChannelStereoPair (int index) const;
 
-
+    //! Returns true if the processor wants midi messages. 
     bool acceptsMidi() const;
+    //! Returns true if the processor produces midi messages. 
     bool producesMidi() const;
+    //! Returns true if a silent input always produces a silent output.
     bool silenceInProducesSilenceOut() const;
+    //! Projects sphere coords to polar coords 
     Point<float> domeToScreen(Point<float>);
     //==============================================================================
+    //! Returns the number of preset programs the filter supports. 
     int getNumPrograms();
+    //! Returns the number of the currently active program.
     int getCurrentProgram();
+    //! Called by the host to change the current program. 
     void setCurrentProgram (int index);
+    //! Must return the name of a given program. 
     const String getProgramName (int index);
+    //! Called by the host to rename a program. 
     void changeProgramName (int index, const String& newName);
 
 
     //==============================================================================
+    //! The host will call this method when it wants to save the filter's internal state. 
     void getStateInformation (MemoryBlock& destData);
+    //! This must restore the filter's state from a block of data previously created using getStateInformation(). 
     void setStateInformation (const void* data, int sizeInBytes);
-
+    
+    //! returns the sources
     SoundSource* getSources(){ return _TabSource; }
+    //! returns the number of sources on the screen.
     int getNbrSources() { return _NbrSources; }
-    void setNbrSources(int newValue) { if ( newValue >-1 && newValue < 8) _NbrSources= newValue; }
+    //! Set the number of sources.
+    void setNbrSources(int newValue) { if ( newValue >-1 && newValue < 9) _NbrSources= newValue; }
     
-    
+    //! return the tab position of the selectedSource 
     int getSelectedSource() { return _SelectedSource; }
+    //! Set the selected source 
     void setSelectedSource(int selected){ if ( selected >-1 && selected < 8) _SelectedSource= selected;};
-    
+    //! Returns the Osc Port for the Zirkonium sending
     int getOscPortZirkonium(){return _OscPortZirkonium;}
-    
+    //! Retunrs the Osc port where the iPad messages are received
     String getOscPortIpadIncoming(){ return _OscPortIpadIncoming;}
-    
+    //! Returns the Osc iPad port where we send messages
     String getOscPortIpadOutgoing(){ return _OscPortIpadOutgoing;}
-    
+    //! Returns the iPad's IP address.
     String getOscAddressIpad() {return _OscAddressIpad; }
-    AudioPlayHead::CurrentPositionInfo lastPosInfo;
-     //OSC Port Zirkonium
-    
-    
     
     enum Parameters
     {
@@ -159,20 +182,27 @@ public:
         ZirkOSC_Gain_Param_7,
         totalNumParams
     };
+    //! Send the current state to all the iPad and Zirkonium
     void sendOSCValues();
     //! Getter constrain type
     int getSelectedConstrain();
     //! Setter constrain type
     void setSelectedContrain(int constrain);
-    
+    //! Retunrs true if the Editor has to refresh the Gui.
     bool hasToRefreshGui(){return _RefreshGui;};
+    //! If you want to refresh the Gui from the Processor.
     void setRefreshGui(bool gui) { _RefreshGui = gui;};
+    //! Change the sending OSC port of the zirkonium
     void changeOSCPort(int newPort);
+    //! Send the configuration to the iPad (assignment Position -> id, nbr source)
     void sendOSCConfig();
+    //! Send the movement type, selected constrain.
     void sendOSCMovementType();
-    
+    //! Change the sending OSC port of the iPad
     void changeOSCSendIPad(int newPort, String newAddress);
+    //! Change the receiving OSC port (server).
     void changeOSCPortReceive(int port);
+    //! Returns the Editor.
     AudioProcessorEditor* getEditor() {return _Editor;};
     
 private:
@@ -181,26 +211,32 @@ private:
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ZirkOscjuceAudioProcessor)
+    //! If the editor has to refresh the GUI
     bool _RefreshGui = false;
-    
+    //! Current number of sources on the screnn
     int _NbrSources;
+    //! ID of the selected constrain
     int _SelectedConstrain=1;
+    //! Tab position of the selected source
     int _SelectedSource;
-    
+    //! Osc port to send to the Zirkonium 
     int _OscPortZirkonium;
-    
+    //! The editor
     AudioProcessorEditor* _Editor;
-    
+    //! Sources tab
     SoundSource _TabSource [8];
+    //! Osc Sever thread (receiving)
     lo_server_thread _St;
+    //! Zirkonium OSC address (sending)
     lo_address _OscZirkonium;
+    //! Ipad OSC address (sending)
     lo_address _OscIpad;
+    //! Outgoing port to the iPad 
     String _OscPortIpadOutgoing = "10112";
+    //! iPad address
     String _OscAddressIpad = "10.0.1.3";
+    //! Zirkonium incoming port
     String _OscPortIpadIncoming = "10114";
-   /* TextButton* button1;
-    Slider* slider1;
-    Label* label;*/
 
 };
 
--- Source/SoundSource.h
+++ Source/SoundSource.h
@@ -16,27 +16,45 @@ public:
     SoundSource();
     SoundSource(float,float);
     ~SoundSource();
-    //getter setter;
+    //! returns the XY position in a Point <float>.
     Point<float> getPositionXY();
+    //! returns the channel (id in the Zirkonium)
     int     getChannel();
+    //! sets the channel (id in the Zirkonium)
     void    setChannel(int);
+    //! returns the X position
     float   getX();
-    void    setPositionXY(Point <float>);
+    //! retunrs the Y position
     float   getY();
+    //! sets the XY position (converts it in azimuth / elevation)
+    void    setPositionXY(Point <float>);
+    //! returns the gain [0,1]
     float   getGain();
+    //! sets the gain 
     void    setGain(float);
+    //! gets the Azimuth [0,1]
     float   getAzimuth();
+    //! sets the azimuth
     void    setAzimuth(float);
+    //! returns the azimuth span
     float   getAzimuthSpan();
+    //! sets the azimuth span
     void    setAzimuthSpan(float);
+    //! returns the elevation span
     float   getElevationSpan();
+    //! sets the elevation span
     void    setElevationSpan(float);
+    //! returns the elevation [0,1]
     float   getElevation();
+    //! returns the elevation [-1,1] form memory.
     float   getElevationRawValue();
+    //! set the elevation 
     void    setElevation(float);
     //! returns true if the point is inside the source. Point is relative to the center of the dome
     bool    contains(Point<float>);
+    //! returns true if the azimuth has been reversed (elevation >1)
     bool    isAzimReverse();
+    //! set true if the azimuth will be reversed
     void    setAzimReverse(bool);
         
 private:
