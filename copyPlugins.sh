0ff50f387f58966c716bc6d65555a88503ec202d
Vincent Berthiaume <vberthiaume@gmail.com>
2015-03-26 12:55:44 -0400

    added vst3 in introjucer, although it is not produced...

---
+++ copyPlugins.sh
@@ -0,0 +1,3 @@
+cp -r /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC2.component ~/Desktop
+cp -r /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC2.vst ~/Desktop
+echo "vst and component moved to ~/Desktop"
