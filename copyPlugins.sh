3d71e5bd11bf776ef7540e4e09a93942259f08af
Vincent Berthiaume <vberthiaume@gmail.com>
2015-08-10 11:29:06 -0400

    change zirkosc2 to 3 in copyPlugin.sh

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -1,3 +1,3 @@
-cp -r /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC2.component ~/Desktop
-cp -r /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC2.vst ~/Desktop
+cp -r /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Desktop
+cp -r /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst ~/Desktop
 echo "vst and component copied to ~/Desktop"
