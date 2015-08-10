cb2123f6968a34a919e44244779e9968a3e89b19
Vincent Berthiaume <vberthiaume@gmail.com>
2015-08-10 11:52:33 -0400

    copyPlugins working but keeping original folder hierarchy

--- copyPlugins.sh
+++ copyPlugins.sh
@@ -1,3 +1,8 @@
-cp -r /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Desktop
-cp -r /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst ~/Desktop
+#!/bin/bash
+
+# cp -r /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC3.component ~/Desktop
+# cp -r /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst ~/Desktop
+
+zip -r ~/Desktop/ZirkOSC.zip /Users/nicolai/Library/Audio/Plug-Ins/Components/ZirkOSC3.component /Users/nicolai/Library/Audio/Plug-Ins/VST/ZirkOSC3.vst 
+
 echo "vst and component copied to ~/Desktop"
