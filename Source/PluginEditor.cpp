3e5b518aa76efb7c37cb4b3598c2ec0a2233d64d
Vincent Berthiaume <vberthiaume@gmail.com>
2015-12-09 14:57:08 -0500

    commented font code for version label

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -347,7 +347,7 @@ ZirkOscAudioProcessorEditor::ZirkOscAudioProcessorEditor (ZirkOscAudioProcessor*
     
 //    Font font("Adobe", 20.f);
     
-    m_VersionLabel.setFont (Font (Font::getDefaultMonospacedFontName(), 12.0f, Font::plain));
+//    m_VersionLabel.setFont (Font (Font::getDefaultMonospacedFontName(), 12.0f, Font::plain));
 
    	m_logoImage.setImage(ImageFileFormat::loadFrom (BinaryData::logoGris_png, (size_t) BinaryData::logoGris_pngSize));
     
